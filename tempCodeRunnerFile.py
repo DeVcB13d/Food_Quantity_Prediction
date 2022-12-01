1f027df2db2a61a414356c5ac8051004a705aaec
abhin2002 <abhinajith40@gmail.com>
2022-12-01 23:18:08 +0530

    500 rows

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+if(int(value)>=250 and int(value)<=270):
+        #     temp_waste = rd.randrange()
--- waste.py
+++ waste.py
@@ -1,5 +1,6 @@
 import csv
 import random as rd
+import decimal
 
 # opening the CSV file
 with open('foodCsv.csv', mode ='r')as file:
@@ -10,6 +11,8 @@ with open('foodCsv.csv', mode ='r')as file:
     for lines in csvFile:
         value=lines[1]
         amount=lines[2]
-        print(value)
-        if(value>=250 and value<270):
-            temp=rd.randrange((amount/10),(amount/5))
+        if(int(value)>=250 and int(value)<=270):
+            temp_waste = float(decimal.Decimal(rd.randrange(10,30))/10)
+        else:
+            temp_waste = float(decimal.Decimal(rd.randrange(30,40))/10)
+        print(temp_waste)
