e17cb752f710dc7bdea27a4e4bca81028b5ef49a
abhin2002 <abhinajith40@gmail.com>
2022-12-02 00:36:58 +0530

    500 wastes added

--- waste.py
+++ waste.py
@@ -3,16 +3,15 @@ import random as rd
 import decimal
 
 # opening the CSV file
-with open('foodCsv.csv', mode ='r')as file:
+with open('newCSV1.csv', mode ='r')as file:
 # reading the CSV file
     csvFile = csv.reader(file)
 
 # displaying the contents of the CSV file
     for lines in csvFile:
         value=lines[1]
-        amount=lines[2]
         if(int(value)>=250 and int(value)<=270):
-            temp_waste = float(decimal.Decimal(rd.randrange(10,30))/10)
+            temp_waste = float(decimal.Decimal(rd.randrange(0,10))/10)
         else:
-            temp_waste = float(decimal.Decimal(rd.randrange(30,40))/10)
+            temp_waste = float(decimal.Decimal(rd.randrange(10,11))/10)
         print(temp_waste)
