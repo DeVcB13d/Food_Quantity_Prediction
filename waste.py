d8777657c866af7e155e4711d805068ef612cfd2
abhin2002 <abhinajith40@gmail.com>
2022-12-02 08:10:20 +0530

    fanal data set

--- waste.py
+++ waste.py
@@ -11,7 +11,7 @@ with open('newCSV1.csv', mode ='r')as file:
     for lines in csvFile:
         value=lines[1]
         if(int(value)>=250 and int(value)<=270):
-            temp_waste = float(decimal.Decimal(rd.randrange(0,10))/10)
+            temp_waste = float(decimal.Decimal(rd.randrange(10,90))/10)
         else:
-            temp_waste = float(decimal.Decimal(rd.randrange(10,11))/10)
+            temp_waste = float(decimal.Decimal(rd.randrange(90,160))/10)
         print(temp_waste)
