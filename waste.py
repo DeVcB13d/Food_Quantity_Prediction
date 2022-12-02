669ba5fb883ba528967cac709300cdabcfdd064e
abhin2002 <abhinajith40@gmail.com>
2022-12-02 08:32:12 +0530

    2

--- waste.py
+++ waste.py
@@ -11,7 +11,7 @@ with open('newCSV1.csv', mode ='r')as file:
     for lines in csvFile:
         value=lines[1]
         if(int(value)>=250 and int(value)<=270):
-            temp_waste = float(decimal.Decimal(rd.randrange(10,90))/10)
+            temp_waste = float(decimal.Decimal(rd.randrange(00,80))/10)
         else:
-            temp_waste = float(decimal.Decimal(rd.randrange(90,160))/10)
+            temp_waste = float(decimal.Decimal(rd.randrange(80,150))/10)
         print(temp_waste)
